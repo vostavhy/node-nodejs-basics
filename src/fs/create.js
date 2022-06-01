5105c3c6d8eefedc115826f2b934d5b1ac02d116
vostavhy <vostavhy@gmail.com>
2022-06-01 20:02:47 +0300

    feat: implement create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,20 @@
+import { fileURLToPath } from 'url';
+import { writeFile } from 'fs/promises';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const create = async () => {
-    // Write your code here 
+  try {
+    await writeFile(
+      path.join(__dirname, 'files', 'fresh.txt'),
+      'I am fresh and young',
+      { flag: 'ax' }
+    );
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
+
+create();
