1ea77582a9160367b02859a95395f04d4f3d5783
vostavhy <vostavhy@gmail.com>
2022-06-02 13:33:56 +0300

    feat: implement list func

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,17 @@
+import { readdir } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { fsError } from '../utils/constants.js';
+import path from 'path';
+
 export const list = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  try {
+    const filesFolder = path.join(__dirname, 'files');
+    const fileNames = await readdir(filesFolder, { withFileTypes: true });
+    console.log(fileNames.map((element) => element.name));
+  } catch {
+    throw fsError;
+  }
 };
