8d980b221fa4f2b3c86ad03d1900a2d5495118bf
vostavhy <vostavhy@gmail.com>
2022-06-02 13:45:19 +0300

    feat: implement read func

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,18 @@
+import { readFile } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { fsError } from '../utils/constants.js';
+import path from 'path';
+
 export const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const fileToRead = path.join(__dirname, 'files', 'fileToRead.txt');
+
+  try {
+    const txt = await readFile(fileToRead, { encoding: 'utf8' });
+    console.log(txt);
+  } catch {
+    throw fsError;
+  }
 };
