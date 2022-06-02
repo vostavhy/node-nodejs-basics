3bad9de11e0a79d0be90f8eae8b11af51f50b696
vostavhy <vostavhy@gmail.com>
2022-06-02 23:22:43 +0300

    feat: implement compress func

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,21 @@
+import { fileURLToPath } from 'url';
+import { createReadStream, createWriteStream } from 'fs';
+import { createUnzip } from 'zlib';
+import { pipeline } from 'stream';
+import { promisify } from 'util';
+import path from 'path';
+
 export const decompress = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const pathToRead = path.join(__dirname, 'files', 'archive.gz');
+  const pathToWrite = path.join(__dirname, 'files', 'fileToCompress.txt');
+
+  const input = createReadStream(pathToRead);
+  const output = createWriteStream(pathToWrite);
+  const unzip = createUnzip();
+
+  pipe = promisify(pipeline);
+  await pipe(input, unzip, output);
 };
