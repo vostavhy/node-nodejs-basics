76b18e08a2fa2ff0ed48cc3f81b571ac23e3acb8
vostavhy <vostavhy@gmail.com>
2022-06-02 17:23:06 +0300

    feat: implement hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,17 @@
+import { createHash } from 'crypto';
+import { fileURLToPath } from 'url';
+import { readFile } from 'fs/promises';
+import path from 'path';
+
 export const calculateHash = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+  const hash = createHash('sha256');
+  const file = await readFile(filePath);
+
+  hash.update(file);
+  return hash.digest('hex');
 };
