665d21af09fde5aa55aeb35c879c02c818541b0a
vostavhy <vostavhy@gmail.com>
2022-06-03 18:06:45 +0300

    feat: implement worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,41 @@
+import { Worker } from 'worker_threads';
+import { fileURLToPath } from 'url';
+import os from 'os';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const pathToWorker = path.join(__dirname, 'worker.js');
+
+const createWorker = (m) => {
+  return new Promise((resolve, reject) => {
+    let w = new Worker(pathToWorker);
+    w.postMessage(m);
+    w.on('message', (msg) => {
+      resolve(msg);
+    });
+    w.on('error', (msg) => {
+      reject(msg);
+    });
+  });
+};
+
 export const performCalculations = async () => {
-    // Write your code here
+  const cpusNumber = os.cpus().length;
+
+  const promises = [];
+
+  for (let i = 0; i < cpusNumber; i++) {
+    promises.push(createWorker(10 + i));
+  }
+
+  let results = await Promise.allSettled(promises);
+  results = results.map(({ status, value }) => {
+    if (status === 'fulfilled') {
+      return { status: 'resolved', data: value };
+    }
+    return { status: 'error', data: null };
+  });
+
+  return results;
 };
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,6 +1,14 @@
-// n should be received from main thread
-export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import { parentPort } from 'worker_threads';
 
-export const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+parentPort.on('message', (msg) => {
+  const fbn = nthFibonacci(msg);
+  sendResult(fbn);
+});
+
+// n should be received from main thread
+export const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+
+export const sendResult = (result) => {
+  parentPort.postMessage(result);
 };
