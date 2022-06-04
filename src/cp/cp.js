d92a3cac2b50986b7f65565f1d3aee66f45e20b1
Nickolay Malysh <stalcrot@yandex.ru>
2022-06-04 09:53:09 +0300

    feat: implement child process task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,14 @@
+import { fork } from 'child_process';
+import { fileURLToPath } from 'url';
+import { stdin, stdout } from 'process';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const pathToScript = path.join(__dirname, 'files', 'script.js');
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+  const childProcess = fork(pathToScript, args.slice(2), { silent: true });
+  stdin.pipe(childProcess.stdin);
+  childProcess.stdout.pipe(stdout);
 };
