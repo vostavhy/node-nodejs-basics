125e3ef29e8b59e0cc333ebe5e2bf859c2b214f1
Nickolay Malysh <stalcrot@yandex.ru>
2022-06-05 04:34:19 +0300

    feat: implement getDir func

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,18 +1,15 @@
 import { readFile } from 'fs/promises';
-import { fileURLToPath } from 'url';
-import { fsError } from '../utils/constants.js';
+import { fsError, getDirName } from '../utils/constants.js';
 import path from 'path';
 
 export const read = async () => {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
-
+  const __dirname = getDirName(import.meta.url);
   const fileToRead = path.join(__dirname, 'files', 'fileToRead.txt');
 
   try {
     const txt = await readFile(fileToRead, { encoding: 'utf8' });
     console.log(txt);
-  } catch {
+  } catch (error) {
     throw fsError;
   }
 };
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,9 @@
 const fsError = new Error('FS operation failed');
+import { fileURLToPath } from 'url';
+import path from 'path';
 
-export { fsError };
+const getDirName = (url) => {
+  return path.dirname(fileURLToPath(url));
+};
+
+export { fsError, getDirName };
